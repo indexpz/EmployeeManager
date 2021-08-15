48cf908031db2715de67d1032c3003eac85b3240
Paweł Zieliński <pawelz72@gmail.com>
2021-08-15 11:32:32 +0200

    Initial commit

---
+++ src/main/java/pl/indexpz/empolyeeeanager/EmpolyeeeanagerApplication.java
@@ -0,0 +1,13 @@
+package pl.indexpz.empolyeeeanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpolyeeeanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpolyeeeanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/indexpz/empolyeeeanager/EmpolyeeeanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.indexpz.empolyeeeanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpolyeeeanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
